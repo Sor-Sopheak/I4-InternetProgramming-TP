7fb2c535f2803c61678a07c91636315019013d0e
SOR SOPHEAK <ssopheak2002@gmail.com>
2023-12-07 12:48:14 +0700

    TP07 JavaScript (NodeJS, TypeScript

---
+++ TP07_3/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var readFile = require('fs').readFile;
+var app = express();
+app.get('/', function (req, res) {
+    readFile('./index.html', 'utf8', function (err, html) {
+        if (err)
+            res.status(500).send("Error");
+        res.send(html);
+    });
+});
+app.get('/detail', function (req, res) {
+    readFile('./detail.html', 'utf-8', function (err, html) {
+        if (err)
+            res.status(500).send("Error");
+        res.send(html);
+    });
+});
+app.listen(3000, function () {
+    console.log("Your app is available on http://localhost:3000");
+});
